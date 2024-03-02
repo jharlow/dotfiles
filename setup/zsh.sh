ae152181507dcc75fc0465943624bb1cd97c75e2
John Harlow <info@jharlow.uk>
2024-03-02 15:21:12 -0800

    fix(nvim): latest nvim version

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -9,7 +9,10 @@ NOCOLOR="$(tput sgr0)"
 function error() { echo -e "$UNDERLINE$RED$1$NOCOLOR\n"; }
 
 # Check that Homebrew is installed
-command -v brew >/dev/null 2>&1 || { error "Homebrew not installed: https://brew.sh/"; exit 1; }
+command -v brew >/dev/null 2>&1 || {
+	error "Homebrew not installed: https://brew.sh/"
+	exit 1
+}
 
 # Ask for the administrator password upfront
 sudo -v
@@ -17,7 +20,7 @@ sudo -v
 brew install zsh
 zsh_path=$(which zsh)
 grep -Fxq "$zsh_path" /etc/shells || sudo bash -c "echo $zsh_path >> /etc/shells"
-chsh -s "$zsh_path" $USER
+chsh -s "$zsh_path" "$USER"
 
 # Install zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
